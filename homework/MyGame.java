12ae740df557051883ab46401789203597a580e9
D3vL4dy <kangji1218@kakao.com>
2022-01-26 22:50:05 +0900

    arrival 미완성

--- homework/MyGame.java
+++ homework/MyGame.java
@@ -3,10 +3,46 @@ package homework;
 import e_oop.ScanUtil;
 
 public class MyGame { // 게임 내의 행위
-	String winner; //사용자가 입력한 정답
-	Runner[] answer; //실제 정답
+
+	String winner; // 사용자가 입력한 정답
+	String[] answer; // 실제 정답
 	final int DISTANCE = 50; // 결승선
-//		Runner r = new Runner();
+	int time = 0; // 시간
+
+	// 10m마다 거리 표시
+	String[] meter = new String[5];
+
+	void meter() {
+		for (int i = 0; i < meter.length; i++) {
+			meter[i] = "       " + ((i + 1) * 10) + "m";
+			System.out.print(meter[i]);
+		}
+		System.out.println();
+	}
+
+	// 선수의 위치를 레인 위에 표시
+	void running_lane(Runner runner) {
+
+		for (int i = 0; i < 50; i++) {
+			runner.lane[i] = "=";
+		}
+		if ((runner.distance != 0) && (runner.distance % 10 == 0)) {
+			runner.lane[runner.distance - 1] = "0";
+		}
+		for (int i = 0; i < 50; i++) {
+			System.out.print(runner.lane[i]);
+		}
+		System.out.println("\n");
+	}
+
+	// 3초마다 10m씩 이동
+	private void stop() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	// 선수가 달릴 수 있는지 상태 점검
 	boolean check_status(Runner runner) { // boolean으로 해당 선수가 달릴 수 있는지 없는지를 판단
@@ -33,255 +69,123 @@ public class MyGame { // 게임 내의 행위
 	}
 
 	// 선수의 위치와 상태 출력 (10~40)
-	void print_distance_40(Runner runner) {
-		runner.distance += 10;
-		System.out.println(runner.name + " 님이 " + runner.distance + "m지점에 도착했습니다.");
-		System.out.println("남은 거리는 " + (DISTANCE - runner.distance) + "m입니다.");
-		runner.hp -= 1;
-		runner.bodyMoisture -= 1;
-		System.out.println("체력 (-1): " + runner.hp + "/5");
-		System.out.println("체내수분량 (-1): " + runner.bodyMoisture + "/10");
-		System.out.println("\n");
+	void print_distance_info_40(Runner[] runner) {
+		stop();
+		System.out.println("\n\n\n<" + time + "초> 지났습니다.\n");
+		for (int i = 0; i < runner.length; i++) {
+			if (check_status(runner[i])) { // 달릴 수 있는 상태
+				runner[i].distance += 10; // 현재 위치, 남은 거리와 능력 출력
+				System.out.println(runner[i].name + " 님이 " + runner[i].distance + "m지점에 도착했습니다.");
+				System.out.println("남은 거리는 " + (DISTANCE - runner[i].distance) + "m입니다.");
+				runner[i].hp -= 1;
+				runner[i].bodyMoisture -= 1;
+				System.out.println("체력 (-1): " + runner[i].hp + "/5");
+				System.out.println("체내수분량 (-1): " + runner[i].bodyMoisture + "/10");
+				System.out.println("\n");
+			} else {
+				System.out.println(runner[i].name + " 님의 현재 위치는 " + runner[i].distance + "m 입니다.");
+				System.out.println("\n...충전중...\n\n");
+				rest(runner[i]); //
+			}
+		}
+		meter();
+		for (int i = 0; i < runner.length; i++) {
+			running_lane(runner[i]);
+		}
+		System.out.println();
 	}
 
 	// 선수의 위치와 상태 출력 (50부터~)
-	void print_distance_50(Runner runner) {
-
-		if (check_status(runner)) { // 달릴 수 있는 상태
-			runner.distance += 10;
-			System.out.println(runner.name + " 님이 " + runner.distance + "m지점에 도착하였습니다.");
-			if (runner.distance == DISTANCE) { // 결승선 도착
-				System.out.println("축하합니다~!\n\n");
-			} else {
-				runner.hp -= 1;
-				runner.bodyMoisture -= 1;
-				System.out.println("남은 거리는 " + (DISTANCE - runner.distance) + "m입니다.");
-				System.out.println("체력 (-1) : " + runner.hp + "/5");
-				System.out.println("체내수분량 (-1) : " + runner.bodyMoisture + "/10\n");
+	void print_distance_info_50(Runner[] runner) {
+		stop();
+		System.out.println("\n\n\n<" + time + "초> 지났습니다.\n");
+		for (int i = 0; i < runner.length; i++) {
+			if (check_status(runner[i])) { // 달릴 수 있는 상태
+				runner[i].distance += 10;
+				System.out.println(runner[i].name + " 님이 " + runner[i].distance + "m지점에 도착하였습니다.");
+				if (runner[i].distance == DISTANCE) { // 결승선 도착
+					System.out.println("축하합니다~!\n\n");
+				} else {
+					runner[i].hp -= 1;
+					runner[i].bodyMoisture -= 1;
+					System.out.println("남은 거리는 " + (DISTANCE - runner[i].distance) + "m입니다.");
+					System.out.println("체력 (-1) : " + runner[i].hp + "/5");
+					System.out.println("체내수분량 (-1) : " + runner[i].bodyMoisture + "/10\n");
+				}
+				System.out.println();
+			} else { // 달릴 수 없는 상태
+				System.out.println(runner[i].name + " 님의 위치는 " + runner[i].distance + "m 입니다.");
+				System.out.println("\n...충전중...\n\n");
+				rest(runner[i]);
 			}
-			System.out.println();
-		} else { // 달릴 수 없는 상태
-			System.out.println(runner.name + " 님의 위치는 " + runner.distance + "m 입니다.");
-			System.out.println("\n...충전중...\n\n");
-			rest(runner);
 		}
-
+		meter();
+		for (int i = 0; i < runner.length; i++) {
+			running_lane(runner[i]);
+		}
+		System.out.println();
+		arrival(runner, answer);
 	}
 
 	// 한명이라도 50m에 도착하면 게임 종료
-	void arrival(Runner[] runner, Runner[] answer) {
+	void arrival(Runner[] runner, String[] answer) {
 		int arrival_num = 0;
 		System.out.println("\n\n");
-
 		for (int i = 0; i < runner.length; i++) {
 			if (runner[i].distance == DISTANCE) { // 한명이라도 50m에 도착하면 true
-				runner[i].arv = true;
-				System.out.print(runner[i] + "  ");
-				answer[i] = runner[i]; //도착한 사람을 정답에 넣어줌
+//				runner[i].arv = true;
+				System.out.println(runner[i].name + "님이 결승선에 도착했습니다.");
+				//================================= 밑부터 코드 안돼
+				answer[arrival_num] = runner[i].name;
 				arrival_num++;
 			}
 		}
+
 		if (0 < arrival_num) {
-			System.out.println("님이 결승선에 도착했습니다.");
-			for(int i = 0; i < arrival_num; i++) {
-				if(winner.equals(answer[i])){
-					System.out.println("정답을 맞췄습니다.  ~~~↖^0^↗~~~");
-				}else {
-					System.out.println("게임 실패!");
+			for (int i = 0; i < answer.length; i++) {
+				System.out.println(answer[i] + "님 도착!!");
+				if (winner.equals(answer[i])) {
+					System.out.println("정답을 맞췄습니다.  ~~~↖^ㅠ^↗~~~");
+					System.out.println("게임을 종료합니다.");
+				} else {
+					System.out.println("게임 실패! ....↙-___-↘....");
+					System.out.println("게임을 다시 시작합니다.");
+					start(runner);
 				}
 			}
 			System.exit(0);
 		}
 	}
-	
-
-	// 10m마다 거리 표시
-	String[] meter = new String[5];
-
-	void meter() {
-		for (int i = 0; i < meter.length; i++) {
-			meter[i] = "       " + ((i + 1) * 10) + "m";
-			System.out.print(meter[i]);
-		}
-		System.out.println();
-	}
-
-	// 선수의 위치를 레인 위에 표시
-	void running_lane(Runner runner) {
-
-		for (int i = 0; i < 50; i++) {
-			runner.lane[i] = "=";
-		}
-		if ((runner.distance != 0) && (runner.distance % 10 == 0)) {
-			runner.lane[runner.distance - 1] = "0";
-		}
-		for (int i = 0; i < 50; i++) {
-			System.out.print(runner.lane[i]);
-		}
-		System.out.println("\n\n");
-	}
-
-	// 3초마다 10m씩 이동
-	private void stop() {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
 
 	// 10초마다 선수의 상태정보를 출력하는 반복문
 	void run(Runner[] runner) { // Runner라는 클래스로 만들어진 배열을 인자(runner)로 받음
-		int time = 0; // 시간
+
 		while (runner[0].distance <= DISTANCE || runner[1].distance <= DISTANCE || runner[2].distance <= DISTANCE
 				|| runner[3].distance <= DISTANCE) {
 
 			time += 10;
 
-			if (time == 10) { // 10초
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					if (check_status(runner[i])) { // 선수가 달릴 수 있으면
-						print_distance_40(runner[i]); // 현재 위치, 남은 거리와 능력 출력
-					} else {
-						System.out.println(runner[i].name + " 님의 현재 위치는 " + runner[i].distance + "m 입니다.");
-						System.out.println("\n...충전중...\n\n");
-						rest(runner[i]); //
-					}
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-			}
+			switch (time) {
+			case 10:
+			case 20:
+			case 30:
+			case 40:
+				print_distance_info_40(runner);
+				break;
 
-			if (time == 20) { // 20초
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					if (check_status(runner[i])) { // 선수가 달릴 수 있으면
-						print_distance_40(runner[i]); // 현재 위치, 남은 거리와 능력 출력
-					} else {
-						System.out.println(runner[i].name + " 님의 현재 위치는 " + runner[i].distance + "m 입니다.");
-						System.out.println("\n...충전중...\n\n");
-						rest(runner[i]); //
-					}
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-			}
-
-			if (time == 30) { // 30초
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					if (check_status(runner[i])) { // 선수가 달릴 수 있으면
-						print_distance_40(runner[i]); // 현재 위치, 남은 거리와 능력 출력
-					} else {
-						System.out.println(runner[i].name + " 님의 현재 위치는 " + runner[i].distance + "m 입니다.");
-						System.out.println("\n...충전중...\n\n");
-						rest(runner[i]); //
-					}
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-			}
-
-			if (time == 40) { // 40초
-//					stop();
-//					System.out.println("\n\n<" + time + "초> 지났습니다.");
-//					meter();
-//					for (int i = 0; i < runner.length; i++) {
-//						running_lane(runner[i]);
-//					}
-//					System.out.println();
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					if (check_status(runner[i])) { // 선수가 달릴 수 있으면
-						print_distance_40(runner[i]); // 현재 위치, 남은 거리와 능력 출력
-					} else {
-						System.out.println(runner[i].name + " 님의 현재 위치는 " + runner[i].distance + "m 입니다.");
-						System.out.println("\n...충전중...\n\n");
-						rest(runner[i]); //
-					}
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-			}
-
-			if (time == 50) { // 50초
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					print_distance_50(runner[i]);
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-				arrival(runner, answer);
-			}
-
-			if (time == 60) { // 60초
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					print_distance_50(runner[i]);
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-				arrival(runner, answer);
-			}
-
-			if (time == 70) {
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					print_distance_50(runner[i]);
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-				arrival(runner, answer);
-			}
-			
-			if (time == 80) {
-				stop();
-				System.out.println("\n\n\n\n<" + time + "초> 지났습니다.\n");
-				for (int i = 0; i < runner.length; i++) {
-					print_distance_50(runner[i]);
-				}
-				meter();
-				for (int i = 0; i < runner.length; i++) {
-					running_lane(runner[i]);
-				}
-				System.out.println();
-				arrival(runner, answer);
+			case 50:
+			case 60:
+			case 70:
+			case 80:
+				print_distance_info_50(runner);
+				break;
 			}
 		}
-
 	}
+
 	String check() {
 		System.out.println("어느 선수가 1등일까요? 맞춰주세요!");
-		String winner = ScanUtil.nextLine();
+		winner = ScanUtil.nextLine();
 		return winner;
 	}
 
@@ -314,7 +218,7 @@ public class MyGame { // 게임 내의 행위
 				break;
 			case 0:
 				System.out.println("게임이 종료되었습니다.");
-				System.exit(0); // 프로그램을 강제종료시키는 메소드
+				System.exit(0);
 			}
 		}
 	}
@@ -350,8 +254,8 @@ public class MyGame { // 게임 내의 행위
 			runner[i].name = Names[i];
 		}
 
-		MyGame game1 = new MyGame();
-		game1.start(runner);
+		MyGame mg = new MyGame();
+		mg.start(runner);
 
 		new MyGame().start(runner);
 	}
--- homework/Runner.java
+++ homework/Runner.java
@@ -1,23 +1,82 @@
 package homework;
 
-public class Runner { // Setting 클래스의 초기화블럭, 생성자를 제외한 나머지 상속받음
+public class Runner {
 
 	String name;
-	int hp = (int) (Math.random() * 5) + 1; // 체력 1~5까지
-	int bodyMoisture = (int) (Math.random() * 4) + 2; // 체내수분량 2~5 까지
+	int hp = (int) (Math.random() * 4) + 1; // 체력 1~4까지
+	int bodyMoisture = (int) (Math.random() * 4) + 1; // 체내수분량 1~4 까지
 	int distance = 0; // 거리
+	
 
 	boolean is_taking_rest = true; // 휴식중
-	boolean arv = false;
+//	boolean arv = false; // 결승선에 들어온 선수가 있는지 확인하는 변수
 
 	String[] lane = new String[50];
-	
-	
-	
 
 	@Override
 	public String toString() {
 		return name;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	void arrival(Runner[] runner){
+//		int winner_num = 0;
+//		
+//		for(int i = 0; i < runner.length; i++) {
+//			if (runner[i].distance == DISTANCE) { //한명이라도 50m에 도착하면 true
+//				runner[i].arv = true;
+//				System.out.println(runner[i]+"님이 결승선에 도착한 관계로 게임을 종료합니다.");
+//				winner_num ++;
+//			}
+//		}
+//		if(0 < winner_num) {
+//			System.exit(0);
+//		}
+//	}
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
